a73dff82ddf49a82275bf1b9b5a447392f0b35cd
Mike D <mikedaug@gmail.com>
2017-10-23 15:57:58 -0500

    updated template to generic form

--- test.py
+++ test.py
@@ -1,19 +1,15 @@
 ## demo from http://www.onlamp.com/pub/a/python/2004/12/02/tdd_pyunit.html
+## unittest template
 
 import unittest
 
 class FooTests(unittest.TestCase):
 
-    def testMatches(self):
-        p = DatePattern(2004, 9, 28)
-        d = datetime.date(2004, 9, 28)
-        self.failUnless(p.matches(d))
+    def testFoo(self):
+        self.failUnless(False)
 
 def main():
     unittest.main()
 
 if __name__ == '__main__':
     main()
-
-
-
